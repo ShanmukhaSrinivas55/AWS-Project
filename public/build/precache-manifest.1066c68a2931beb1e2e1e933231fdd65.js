4f5456893de6095197dc731e877d20819750d12c
ShanmukhaSrinivas55 <shanmukhasrinivas575@gmail.com>
2021-01-25 20:13:15 +0530

    fourth commit

---
+++ public/build/precache-manifest.1066c68a2931beb1e2e1e933231fdd65.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc5806353eb56de53a5c1d3879a8cca8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5657cf4b6e1379304eee",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "916843767d17066b13e4",
+    "url": "/static/css/main.ec2274a3.chunk.css"
+  },
+  {
+    "revision": "5657cf4b6e1379304eee",
+    "url": "/static/js/2.7d3370e1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.7d3370e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "916843767d17066b13e4",
+    "url": "/static/js/main.0dddd041.chunk.js"
+  },
+  {
+    "revision": "9091f47bb88943cb9e61",
+    "url": "/static/js/runtime-main.89d020cb.js"
+  },
+  {
+    "revision": "e012fef6182cfdd6a0da06b9be8f25cf",
+    "url": "/static/media/im3.e012fef6.jpg"
+  }
+]);
