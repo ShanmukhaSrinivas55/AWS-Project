7011444c373a19f36ea329930ac1a118605b39e2
ShanmukhaSrinivas55 <shanmukhasrinivas575@gmail.com>
2021-01-25 23:53:44 +0530

    b commit

--- omdb-ui/src/App.js
+++ omdb-ui/src/App.js
@@ -19,8 +19,7 @@ function App() {
         <Switch>
           <Route path="/search" component={SearchPage} />
           <Route path="/details/:imdbID" component={DetailsPage} />
-          <Route path="/" component={HomePage} />
-          <Redirect to = "/search" />
+          <Route path="/*" component={SearchPage} />
         </Switch>
       </Router>
     </div>
