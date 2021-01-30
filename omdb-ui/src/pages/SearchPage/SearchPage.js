169cf667431a680fb014a22320a742d593eea02b
ShanmukhaSrinivas55 <shanmukhasrinivas575@gmail.com>
2021-01-30 22:41:06 +0530

    commit three

--- omdb-ui/src/pages/SearchPage/SearchPage.js
+++ omdb-ui/src/pages/SearchPage/SearchPage.js
@@ -19,7 +19,8 @@ export default function SearchPage() {
   }
 
   const onSubmit = (searchTerm) => {
-    const uri = `${baseUrl}omdb/search?title=${searchTerm}`;
+    // const uri = `${baseUrl}omdb/search?title=${searchTerm}`;
+    const uri = `${baseUrl}search?title=${searchTerm}`;
     axios.get(uri, axiosHeaders)
       .then(res => onSubmitSuccess(res))
       .catch(err => console.error(err));
